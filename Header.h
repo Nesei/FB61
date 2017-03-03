36c594f83f7b85415527899b569dc6af5311e718
Vlad Likhosherst <neinryl@gmail.com>
2017-03-03 14:01:21 +0200

    Update Header.h

--- Header.h
+++ Header.h
@@ -31,3 +31,50 @@ private:
 	int *mark = new int[nm];
 	void WatchMarks();
 };
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Student
+{
+protected:
+	string vnz;
+	string name;
+	int age;
+	void SetVNZ();
+	void OutData();
+	void OutAge();
+	void ChangeName();
+	void ChangeAge();
+public:
+	Student();
+	Student(string, int, string);
+	void Interface();
+};
+
+class Journal : private Student
+{
+public:
+	void Interface();
+	Journal();
+	~Journal();
+private:
+	int nm;
+	int *mark = new int[nm];
+	void WatchMarks();
+};
+
+class halyavschiki:private Student
+{
+	halyavschiki(string,int,string):Student(string,int,string)
+	{}
+	void Interface();
+	void halyava(string halyava)
+	{
+		cout<<"Nu i xto halyavschiki?"<<endl;
+		cin>>halyava;
+		
+	}
+	
+};
